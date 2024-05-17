bd4c53157b15c7f9e5ee0ede1cc64915122b0aa9
Polina Golodaevskaya <golodaevskaya@gmail.com>
2024-05-17 19:54:46 +0300

    4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,7 @@ dependencies {
     implementation ("com.github.dhaval2404:imagepicker:2.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
---
+++ app/src/main/java/com/example/bouqetflowershop/User.java
@@ -0,0 +1,13 @@
+package com.example.bouqetflowershop;
+
+public class User {
+    String id, name, second_name, phone_number, email;
+
+    public User(String id, String name, String second_name, String phone_number, String email) {
+        this.id = id;
+        this.name = name;
+        this.second_name = second_name;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+}
